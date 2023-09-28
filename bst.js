e2aaa98d7ed4e7784f1121d126323621ec8908c0
ColbyCabrera <gdthyispro@gmail.com>
2023-09-28 12:41:24 -0400

    Correct spelling errors

--- bst.js
+++ bst.js
@@ -1,19 +1,24 @@
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class Tree {
-    contructor(array) {
-        array.sort();
-        console.log(array);
-        this.root = buildTree(array);
-    }
+  constructor(array) {
+    array.sort();
+    console.log(array);
+    this.root = this.buildTree(array);
+  }
 
-    buildTree(array) {
-
-    }
+  buildTree(array) {
+    const root = new Node(array[array.length / 2]);
+    root.left = new Node(this.buildTree())
+    return root;
+  }
 }
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+console.log(tree.root);
