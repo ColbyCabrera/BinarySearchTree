54bbdf78678a8315bb61d021483b93b54b9cf179
ColbyCabrera <gdthyispro@gmail.com>
2023-10-02 11:46:27 -0400

    Add pretty print function

--- bst.js
+++ bst.js
@@ -14,11 +14,30 @@ class Tree {
   }
 
   buildTree(array) {
-    const root = new Node(array[array.length / 2]);
-    root.left = new Node(this.buildTree())
+
+    if (array.length == 1) {
+        return new Node(array[0]);
+    }
+    const mid = array.length / 2;
+    const root = new Node(array[mid]);
+    root.left = this.buildTree(array.slice(0, mid));
+    root.right = this.buildTree(array.slice(mid));
     return root;
   }
 }
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(tree.root);
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 11, 12, 67, 6345, 324]);
+prettyPrint(tree.root)
